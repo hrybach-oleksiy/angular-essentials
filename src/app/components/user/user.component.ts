854ef48f218d0504ffa3308fe6df85e0a229a79c
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-12-27 23:25:42 +0200

    feat: add user to the page

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../../../dummy-users';
+import { getRandomIndex } from '../../../helpers/getRandomIndex';
+import User from '../../../types/interfaces';
 
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
+  public selectedUser: User = DUMMY_USERS[getRandomIndex(DUMMY_USERS)];
 
+  public get imagePath(): string {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
 }
